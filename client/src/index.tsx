c8fcff02b7e0a587f782e46b5f34d4f9053bdc26
Ahmed Tabib <tebib.ahmed0@gmail.com>
2024-05-03 21:05:01 +0200

    feat(client): change js to ts

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,19 +1,25 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.scss";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './assets/css/App.css';
+import { HashRouter, Route, Switch, Redirect } from 'react-router-dom';
+import AuthLayout from './layouts/auth';
+import AdminLayout from './layouts/admin';
+import RTLLayout from './layouts/rtl';
+import { ChakraProvider } from '@chakra-ui/react';
+import theme from './theme/theme';
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+ReactDOM.render(
+	<ChakraProvider theme={theme}>
+		<React.StrictMode>
+			<HashRouter>
+				<Switch>
+					<Route path={`/auth`} component={AuthLayout} />
+					<Route path={`/admin`} component={AdminLayout} />
+					<Route path={`/rtl`} component={RTLLayout} />
+					<Redirect from='/' to='/admin' />
+				</Switch>
+			</HashRouter>
+		</React.StrictMode>
+	</ChakraProvider>,
+	document.getElementById('root')
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
